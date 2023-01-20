602b4cc4a31e3ffba4a74612dd59ce89ce328e4e
CedricJablonka <cedricjablonka@gmail.com>
2023-01-20 01:25:54 +0100

    changed the properties of a street

---
+++ client/src/constants/test2.js
@@ -0,0 +1,35 @@
+var test = {
+  streetDetailData: {
+    __v: 0,
+    axleType: "",
+    hasValues: true,
+    installationYear: "2020",
+    state: "",
+    streetCategory: "",
+    usage: "test",
+    streetName: "L 654",
+  },
+  plainData: [
+    {
+      area: "10",
+      thickness: "2",
+      layerType: "",
+      mass: "20",
+      _id: "63c7193a372fef5d78efad05",
+    },
+    {
+      area: "45",
+      thickness: "23",
+      layerType: "",
+      mass: "1035",
+      _id: "63c7193a372fef5d78efad06",
+    },
+    {
+      area: "",
+      thickness: "",
+      layerType: "",
+      mass: "",
+      _id: "63c9d4dcab9253f3a91a7587",
+    },
+  ],
+};
